e968368047602b29142a4c094a389a496b0a97aa
jonoromo <jsromeo@calpoly.edu>
2024-02-20 09:10:50 -0800

    main

--- src/main.py
+++ src/main.py
@@ -14,22 +14,47 @@ import gc
 import pyb
 import cotask
 import task_share
-
-
+import motor_driver
+import encoder_reader
+import motor_controller
+import utime
+        
 def task1_fun(shares):
     """!
     Task which puts things into a share and a queue.
     @param shares A list holding the share and queue used by this task
     """
     # Get references to the share and queue which have been passed to this task
-    my_share, my_queue = shares
+    enPin = pyb.Pin(pyb.Pin.board.PA10, pyb.Pin.OUT_PP) # Initialize pin en_pin (PA10)
+    in2_pin = pyb.Pin(pyb.Pin.board.PB4, pyb.Pin.OUT_PP) # Initialize pin in2_pin (PB5)
+    in1_pin = pyb.Pin(pyb.Pin.board.PB5, pyb.Pin.OUT_PP) # Initialize pin in1_pin (PB4)
+    timmy = pyb.Timer(3, freq=20000) # Initialize timer
+    ch_pos = timmy.channel(2, pyb.Timer.PWM, pin=in2_pin) # Initialize positive direction timer channel
+    ch_neg = timmy.channel(1, pyb.Timer.PWM, pin=in1_pin) # Initialize negative direction timer channel
+    
+    moe = motor_driver.MotorDriver(enPin, in2_pin, in1_pin, timmy, ch_pos, ch_neg)
+    
+    pinA = pyb.Pin(pyb.Pin.board.PC6, pyb.Pin.AF_PP, pull=pyb.Pin.PULL_NONE, af=pyb.Pin.AF1_TIM2)
+    pinB = pyb.Pin(pyb.Pin.board.PC7, pyb.Pin.AF_PP, pull=pyb.Pin.PULL_NONE, af=pyb.Pin.AF1_TIM2)
 
-    counter = 0
+    timer = pyb.Timer(8, prescaler=1, period=65535)
+    chan_A = timer.channel(1, pyb.Timer.ENC_AB, pin=pinA)
+    chan_B = timer.channel(2, pyb.Timer.ENC_AB, pin=pinB)
+    
+    enc = encoder_reader.Encoder(pinA, pinB, timer, chan_A, chan_B)
+    
+    con = motor_controller.Controller(0.1, 10000)
+    setpoint = 0
+    
     while True:
-        my_share.put(counter)
-        my_queue.put(counter)
-        counter += 1
-
+        setpoint += 10000
+        for i in range(60):
+            moe.set_duty_cycle(con.run(setpoint,enc.read()))
+            con.meas_time(utime.ticks_ms())
+            con.meas_pos(enc.read())
+            yield 0
+        moe.set_duty_cycle(0)   
+        con.print_results()
         yield 0
 
 
@@ -67,9 +92,9 @@ if __name__ == "__main__":
     # allocated for state transition tracing, and the application will run out
     # of memory after a while and quit. Therefore, use tracing only for 
     # debugging and set trace to False when it's not needed
-    task1 = cotask.Task(task1_fun, name="Task_1", priority=1, period=400,
+    task1 = cotask.Task(task1_fun, name="Task_1", priority=1, period=10,
                         profile=True, trace=False, shares=(share0, q0))
-    task2 = cotask.Task(task2_fun, name="Task_2", priority=2, period=1500,
+    task2 = cotask.Task(task2_fun, name="Task_2", priority=2, period=50,
                         profile=True, trace=False, shares=(share0, q0))
     cotask.task_list.append(task1)
     cotask.task_list.append(task2)
